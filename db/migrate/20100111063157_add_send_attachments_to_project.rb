305454e0b70515180c90b97f3c130ebb467696a7
serg <serg@serg.(none)>
2010-01-11 09:47:04 +0300

    Add column migration AddSendAttachmentsToProject

---
+++ db/migrate/20100111063157_add_send_attachments_to_project.rb
@@ -0,0 +1,9 @@
+class AddSendAttachmentsToProject < ActiveRecord::Migration
+  def self.up
+    add_column :projects, :send_attachments, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :projects, :send_attachments
+  end
+end
