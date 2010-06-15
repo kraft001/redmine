d01663119e902db19c2beb3f153ce42f82513352
serg <serg@serg.(none)>
2010-06-15 12:32:36 +0400

    User model | changed login maxlength limit up to 50 chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,7 @@ class User < Principal
   validates_uniqueness_of :mail, :if => Proc.new { |user| !user.mail.blank? }, :case_sensitive => false
   # Login must contain lettres, numbers, underscores only
   validates_format_of :login, :with => /^[a-z0-9_\-@\.]*$/i
-  validates_length_of :login, :maximum => 30
+  validates_length_of :login, :maximum => 50
   validates_format_of :firstname, :lastname, :with => /^[\w\s\'\-\.]*$/i
   validates_length_of :firstname, :lastname, :maximum => 30
   validates_format_of :mail, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :allow_nil => true
